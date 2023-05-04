bd3e6d6b67ebdc4b78b187151cd5121911fa227f
EduardUngureanu <eduard.ungureanu.3001@gmail.com>
2023-05-04 20:18:58 +0300

    Fixed includes

--- utils.h
+++ utils.h
@@ -1,11 +1,19 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
-#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h> 
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <stdarg.h>
 
 int tap_alloc(char *dev) {
     struct ifreq ifr;
